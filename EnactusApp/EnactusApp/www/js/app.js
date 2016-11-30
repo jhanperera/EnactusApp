f1b9bccff31573080c868f7a4549319fc57023b0
jhanperera <jhanperera@live.com>
2016-11-30 11:27:17 -0500

    adding load animations

--- EnactusApp/EnactusApp/www/js/app.js
+++ EnactusApp/EnactusApp/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('iFinance', ['ionic', 'controllers', 'services'])
+angular.module('iFinance', ['ionic', 'controllers', 'services', 'ngAnimate'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
