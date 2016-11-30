cd7828eb46017c4478f153c9766fea600ff22451
jhanperera <jhanperera@live.com>
2016-11-30 02:40:49 -0500

    added icons, images, and elemtns to main page

---
+++ EnactusApp/EnactusApp/www/js/controllers.js
@@ -0,0 +1,8 @@
+﻿//This creates a new module called 'controllers' that will hold all of the controllers for our app
+angular.module('controllers', [])
+
+.controller('MainCtrl', function ($scope, $state) {
+    
+    console.log('MainCtrl');
+
+});
