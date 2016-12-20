eb52d5edaff5cb66cb68c336f58ba7f52ce4fbd7
Richmond-Truong <Richmond.t@hotmail.ca>
2016-12-19 19:27:55 -0500

    Add files via upload

--- EnactusApp/EnactusApp/www/js/services.js
+++ EnactusApp/EnactusApp/www/js/services.js
@@ -1,1 +1,13 @@
-﻿angular.module('services', []);
+﻿angular.module('services', [])
+	.service('lives', function () {
+        var live = 3;
+
+        return {
+            getlives: function () {
+                return live;
+            },
+            setlives: function(value) {
+                live = value;
+            }
+        };
+    });
