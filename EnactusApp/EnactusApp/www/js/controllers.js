071ec934c7853577035a341aa07e3bf43f62c299
Richmond-Truong <Richmond.t@hotmail.ca>
2016-12-26 00:22:41 -0500

    Add files via upload

--- EnactusApp/EnactusApp/www/js/controllers.js
+++ EnactusApp/EnactusApp/www/js/controllers.js
@@ -11,7 +11,7 @@ angular.module('controllers', ['services'])
 
 })
 .controller("BtnClick", function($scope,lives){
-	var live = lives.getlives();
+	var live = 3;
     $scope.part2Cred = false;
     $scope.part3Cred = false;
     $scope.part4Cred = false;
@@ -20,21 +20,35 @@ angular.module('controllers', ['services'])
     $scope.part7Cred = false;
     $scope.part8Cred = false;
     $scope.part9Cred = false;
+	$scope.part10Cred = false;
+	$scope.partQCred = false;
 	
 	$scope.updatelives = function (){
 		//grabs the element that is called liv then updates it
 		var livesOnPage = document.getElementById('liv');
-		livesOnPage.innerHTML = lives.getlives();
+		livesOnPage.innerHTML = live;
 	}
-	$scope.wrong = function (){
+	$scope.wrong = function (event){
+		var selec = document.getElementById(event.target.id);
+		selec.style.color = "grey";
+	
 		live = live - 1;
-		lives.setlives(live);
-		$scope.updatelives();
+		if(live == 0){
+			$scope.gameover();
+		}
+		else{
+			$scope.updatelives();
+		}
 	}
-	$scope.right = function (){
-		live = live + 1;
-		lives.setlives(live);
-		$scope.updatelives();
+	$scope.right = function (event){
+		var selec = document.getElementById(event.target.id);
+		selec.style.color = "green";
+	}
+	$scope.gameover = function(){
+		alert("game over please try again");
+		live = 3;
+		$scope.partQCred = false;
+		$scope.part1Cred = !$scope.part1Cred;
 	}
 });
 
